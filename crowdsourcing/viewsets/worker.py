3ce59bec07d93150ece9e577140da74b5ce9f671
dmorina <drm.mrn@gmail.com>
2016-05-15 12:02:19 -0700

    include submitted

--- crowdsourcing/viewsets/worker.py
+++ crowdsourcing/viewsets/worker.py
@@ -115,7 +115,8 @@ class WorkerViewSet(viewsets.ModelViewSet):
                                      host='mechanicalturk.amazonaws.com')
         for worker in data:
             tasks = TaskWorker.objects.values('task__project__price', 'id') \
-                .filter(worker__alias=worker['worker_id'], task_status=TaskWorker.STATUS_ACCEPTED, is_paid=False)
+                .filter(worker__alias=worker['worker_id'],
+                        task_status__in=[TaskWorker.STATUS_ACCEPTED, TaskWorker.STATUS_SUBMITTED], is_paid=False)
             total = sum(tasks.values_list('task__project__price', flat=True))
             if total > 0:
                 price = Price(total)
